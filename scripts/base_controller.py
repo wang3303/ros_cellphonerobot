1c7f02c22b14dc3397106a2d6d462ba18118b3c8
agkhalil <elghazalykhalil@gmail.com>
2018-01-30 16:46:13 -0500

    Base controller code updated.

--- scripts/base_controller.py
+++ scripts/base_controller.py
@@ -7,13 +7,16 @@ This node is created to translate cmd_vel commands into motor commands.
 import rospy
 import time
 from hardware import DCmotor
+from geometry_msgs.msg import Twist
 
 def callback(data):
     vx = data.linear.x
     vy = data.linear.y
     th = data.angular.z
     
-    rospy.loginfo("I heard %s", data.data)
+    rospy.loginfo("I heard %s", data)
+    
+    # TODO Convert Twist data to motor commands after encoders are installed 
     
     motor_rf.forward(70)
     motor_lf.reverse(70)
@@ -22,7 +25,10 @@ def callback(data):
 
 def listener():
 	rospy.init_node('base_controller')
+	rospy.Subscriber("cmd_vel", Twist, callback)
+	rospy.spin()
 	
+if __name__ == '__main__':
 	motor = rospy.get_param("/motor")
 	motor_rf_pin = motor['motor_rf'] #rospy.get_param('/execution/'+motor[0])
 	motor_lf_pin = motor['motor_lf'] #rospy.get_param('/execution/'+motor[1])
@@ -33,6 +39,4 @@ def listener():
 	motor_rb = DCmotor(motor_rb_pin[0],motor_rb_pin[1],motor_rb_pin[2])
 	motor_lb = DCmotor(motor_lb_pin[0],motor_lb_pin[1],motor_lb_pin[2])
 	
-	rospy.subscriber("/cmd_vel", geometry_msgs/Twist, callback)
-	
-	rospy.spin()
+	listener()
